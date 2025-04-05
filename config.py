3bca81f4254db53cff6fa36ed469857f32548954
Rthvik Raviprakash <46725716+rravipra@users.noreply.github.com>
2025-04-05 13:26:52 -0700

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,6 @@
 API_KEY = "YOUR_API_KEY_HERE"
 DATA_FILE = "traffic_data.csv"
 LAST_RETRAIN_FILE = "last_retrain.txt"
-# Retraining interval (e.g., 2 weeks)
-RETRAIN_INTERVAL_SECONDS = 3600 * 24 * 14
+
+# Retraining interval
+RETRAIN_INTERVAL_SECONDS = 3600 * 24 * 14 #(2 weeks here)
